0989dc718d531d27eadd1dc4fb80068955bb042b
동건 <ehdrjs1346@naver.com>
2019-01-23 15:26:56 +0900

    Add User interface

---
+++ semiProject/src/semiProject1/UserImpl.java
@@ -0,0 +1,53 @@
+package semiProject1;
+
+public class UserImpl implements User {
+
+	@Override
+	public void signUP() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void logIn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void logOut() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void searchAround() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void searchKeyWord() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void bookMark() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void confirmBook() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void userInfo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
